06c7f2058b04551491f525c13b5944b41e74f845
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:12:23 +0100

    Create laptop class

---
+++ Aplikacja/Laptop.cs
@@ -0,0 +1,21 @@
+﻿namespace Aplikacja
+{
+    public class Laptop : Device
+    {
+        public string ProcessorName { get; set; }
+        public string RamSize { get; set; }
+        
+        public Laptop(string name, int purchasePrice, int productionYear, int rentalPrice, bool isAvailableToRent, string processorName, string ramSize) : base(name, purchasePrice, productionYear, rentalPrice, isAvailableToRent)
+        {
+            ProcessorName = processorName;
+            RamSize = ramSize;
+        }
+        
+        public override string ToString()
+        {
+            string text =
+                $"Laptop {Name} z procesorem {ProcessorName} i ramem {RamSize} GB - wyprodukowany w {ProductionYear}";
+            return text;
+        }
+    }
+}

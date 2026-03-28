2e11c7da7d2470a9e5986f68ccd1c11bb5c0afea
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:13:48 +0100

    Create student class

---
+++ Aplikacja/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace Aplikacja
+{
+    public class Student : User
+    {
+        public override int MaxRentals()
+        {
+            return 2;
+        }
+
+        public Student(string firstName, string lastName) : base(firstName, lastName, "student")
+        {
+            
+        }
+        
+        public override string ToString()
+        {
+            string text =
+                $"student {FirstName} {LastName}";
+            return text;
+        }
+    }
+}

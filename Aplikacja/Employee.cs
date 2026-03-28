45714180d75b93163f2693d3347b5f9cd203d513
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:14:06 +0100

    Create employee class

---
+++ Aplikacja/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace Aplikacja
+{
+    public class Employee : User
+    {
+        public override int MaxRentals()
+        {
+            return 2;
+        }
+
+        public Employee(string firstName, string lastName) : base(firstName, lastName, "employee")
+        {
+            
+        }
+        
+        public override string ToString()
+        {
+            string text =
+                $"pracownik {FirstName} {LastName}";
+            return text;
+        }
+    }
+}

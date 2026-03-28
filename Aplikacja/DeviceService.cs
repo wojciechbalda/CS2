46f1f475e850b0229618dde8b125d84a0edf9d64
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:16:38 +0100

    Create DeviceService class

---
+++ Aplikacja/DeviceService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aplikacja
+{
+    public class DeviceService
+    {
+        private List<Device> devices = new List<Device>();
+        
+        public Device AddDevice(Device device)
+        {
+            devices.Add(device);
+            return device;
+        }
+
+        public Device GetDeviceById(int id)
+        {
+            return devices.FirstOrDefault(device => device.Id == id);
+        }
+        
+        public List<Device> GetAllDevices()
+        {
+            return devices;    
+        }
+        
+        public List<Device> GetAvailableDevices()
+        {
+            return devices.Where(e => e.IsAvailableToRent).ToList();
+        }
+
+        public void SetUnavailableToRentState(int id)
+        {
+            Device device = GetDeviceById(id);
+
+            if (device != null && device.IsAvailableToRent)
+                device.IsAvailableToRent = false;
+            else
+                throw new Exception(
+                    "Nie możesz sprawić, aby urządzenie było niemożliwe do wypożyczenia w momencie, kiedy urządzenie już jest niemożliwe do wypożyczenia lub ktoś obecnie ma je na wypożyczeniu");
+        }
+    }
+}

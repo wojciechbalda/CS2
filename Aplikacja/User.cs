806f8be190e3bfa8de4b572189dea16ac775bbcb
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:13:21 +0100

    Create user class

---
+++ Aplikacja/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Aplikacja
+{
+    abstract public class User
+    { 
+        private static int count = 0;
+        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Type { get; set; }
+        
+
+        public User(string firstName, string lastName, string type)
+        {
+            Id = count++;
+            FirstName = firstName;
+            LastName = lastName;
+            Type = type;
+        }
+
+        public abstract int MaxRentals();
+    }
+}

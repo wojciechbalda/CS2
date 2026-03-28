d8664e0f1d5fec3fe99e7a3798c0d330bcccadf6
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:09:10 +0100

    Create Device class

---
+++ Aplikacja/Device.cs
@@ -0,0 +1,25 @@
+﻿namespace Aplikacja
+{
+    public class Device
+    {
+        private static int count = 0;
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public int PurchasePrice { get; set; }
+        public int ProductionYear { get; set; }
+        public int RentalPrice { get; set; }
+        public bool IsAvailableToRent { get; set; }
+
+        public Device(string name, int purchasePrice, int productionYear, int rentalPrice, bool isAvailableToRent)
+        {
+            Id = count++;
+            Name = name; 
+            PurchasePrice = purchasePrice;
+            ProductionYear = productionYear;
+            RentalPrice = rentalPrice;
+            IsAvailableToRent = isAvailableToRent;
+        }
+    }
+}

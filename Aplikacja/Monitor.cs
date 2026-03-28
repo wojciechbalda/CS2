130ee817e6f7cf402794e77b59e98a9aee380be6
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:11:34 +0100

    Add monitor class

---
+++ Aplikacja/Monitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aplikacja
+{
+    public class Monitor : Device
+    {
+        public int ScreenSizeInInches { get; set; }
+        public int RefreshRate { get; set; }
+
+        public Monitor(string name, int purchasePrice, int productionYear, int rentalPrice, bool isAvailableToRent, int screenSizeInInches, int refreshRate) :
+            base(name, purchasePrice, productionYear, rentalPrice, isAvailableToRent)
+        {
+            ScreenSizeInInches = screenSizeInInches;
+            RefreshRate = refreshRate;
+        }
+
+        public override string ToString()
+        {
+            string text =
+                $"Monitor {Name} {ScreenSizeInInches}'' z odświeżaniem {RefreshRate} Hz - wyprodukowany w {ProductionYear}";
+            return text;
+        }
+    }
+}

2bec05c06a7794ee8ace325ca8166226405dc5fd
wojciechbalda <wojtek.balda@gmail.com>
2026-03-28 23:16:22 +0100

    Create UserService class

---
+++ Aplikacja/UserService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Aplikacja
+{
+    public class UserService
+    {
+        private List<User> users = new List<User>();
+        
+        public User AddUser(string firstName, string lastName, string type)
+        {
+            User user;
+            
+            if (type == "employee")
+                user = new Employee(firstName, lastName);
+            else
+                user = new Student(firstName, lastName);
+            
+            users.Add(user);
+            return user;
+        }
+        
+        public User GetUserById(int id)
+        {
+            return users.FirstOrDefault(user => user.Id == id);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return users;
+        }
+    }
+}
